8ffbd5774c9e7418b3dd1ecf713eaa088bff015a
Gkolman <Gkolman17@gmail.com>
2020-10-27 17:26:05 -0700

    (init)

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,28 +1,7 @@
-var Stack = function() {
-  var someInstance = {};
-  // Use an object with numeric keys to store values
-  var storage = {};
+class Stack {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  constructor() {
+  }
 
-  var size = 0;
-
-  // Implement the methods below
-  someInstance.push = function(value) {
-    storage[size] = value;
-    size++;
-  };
-
-  someInstance.pop = function() {
-    if (size < 1) { return; }
-    var copy = storage[size - 1];
-    delete storage[size - 1];
-
-    size--;
-    return copy;
-  };
-
-  someInstance.size = function() {
-    return size;
-  };
-
-  return someInstance;
-};
+}
